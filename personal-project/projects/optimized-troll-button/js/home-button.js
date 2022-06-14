3320fb7100a4f102d80426f40f45283affa515ad
ZiplEix <baptiste.leroyer@epitech.eu>
2022-06-14 17:44:03 +0200

    [FIX] route and CSS for optimized troll button

--- personal-project/projects/optimized-troll-button/js/home-button.js
+++ personal-project/projects/optimized-troll-button/js/home-button.js
@@ -2,6 +2,6 @@
 let homeButton = document.getElementById('home-button');
 
 homeButton.addEventListener('click', function() {
-    window.location.href = '../index.html';
+    window.location.href = "/";
     }
 );
